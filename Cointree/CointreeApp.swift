b7c524003a2afdb06395257e511d19e3bafdd8fc
Nikita Mounier <nikita.mounier@gmail.com>
2022-09-02 19:49:21 -0400

    Initial Commit

---
+++ Cointree/CointreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CointreeApp.swift
+//  Cointree
+//
+//  Created by Nikita Mounier on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CointreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
